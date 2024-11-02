b2b58a2d8e3146d754df7656f66919cae5d10997
raju_real <rajureal21@gmail.com>
2024-11-02 21:00:40 +0600

    c modified

--- resources/views/website/layouts/menus.blade.php
+++ resources/views/website/layouts/menus.blade.php
@@ -2,4 +2,4 @@
 <li><a href="{{ route('about-us') }}" class="{{ request()->segment(1) === 'about-us' ? 'active' : '' }}">About Us</a></li>
 <li><a href="{{ route('all-events') }}" class="{{ request()->segment(1) === 'all-events' || request()->segment(1) == 'event-details' ? 'active' : '' }}">Events</a></li>
 <li><a href="{{ route('all-articles') }}" class="{{ request()->segment(1) === 'all-articles' || request()->segment(1) == 'article-details' ? 'active' : '' }}">Articles</a></li>
-<li><a href="{{ route('contact-us') }}" {{ request()->segment(1) === 'about-us' ? 'active' : '' }}>Contact Us</a></li>
+<li><a href="{{ route('contact-us') }}" {{ request()->segment(1) === 'contact-us' ? 'active' : '' }}>Contact Us</a></li>
