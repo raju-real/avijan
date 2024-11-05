a79d5319dc2ef5cfa97ee48a8fb9a1a1ce4684d6
raju_real <rajureal21@gmail.com>
2024-11-05 23:32:53 +0600

    policy added

--- resources/views/website/pages/about_us.blade.php
+++ resources/views/website/pages/about_us.blade.php
@@ -48,4 +48,24 @@
             </div>
         </div>
     </section>
+
+    <section id="about" class="about section">
+
+        <!-- Section Title -->
+        <div class="container section-title" data-aos="fade-up">
+            <h2>Privacy Policy</h2>
+        </div><!-- End Section Title -->
+
+        <div class="container">
+            <div class="row gy-3">
+                <div class="col-lg-12 justify-content-center" data-aos="fade-up" data-aos-delay="200">
+                    <div class="about-content ps-0 ps-lg-3">
+                        <p class="fst-italic">
+                            {!! siteSetting()['privacy_policy'] ?? '' !!}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
